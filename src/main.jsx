a54e11b90dccaa9024ab17f89e00cb4cebd89654
chrispretoriusbusiness20-hue <chrispretoriusbusiness20@gmail.com>
2026-06-12 18:46:01 +0200

    Refactor rendering of App component to use createElement

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,5 @@ import './index.css'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>React.StrictMode>,
-  )</React.StrictMode>
+      React.createElement(React.StrictMode, null, React.createElement(App))
+    )
